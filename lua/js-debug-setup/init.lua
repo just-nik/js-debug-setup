5508069f1a09cb32c2e9ab6f858993b837ce0d20
just-nik <ag1499499@gmail.com>
2025-02-06 18:17:06 +0530

    first commit

--- lua/js-debug-setup/init.lua
+++ lua/js-debug-setup/init.lua
@@ -1,1 +1,10 @@
-print("hello world")
+local function get_path(param)
+  local str = package.config:sub(1,1)
+  for _, value in ipairs(param) do
+   str = str .. value .. package.config:sub(1,1)
+   print(str)
+  end
+  return str
+end
+
+print(get_path({"mason","packages"}))
